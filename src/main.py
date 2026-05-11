d196943cf7d179a8d623c17919acd40140b6a055
romeo <romeo.rinaldi1996@gmail.com>
2026-05-11 10:40:19 +0200

    fix key

--- src/main.py
+++ src/main.py
@@ -31,6 +31,9 @@ async def health_check():
 
 
 if __name__ == "__main__":
+    google_api_key = os.environ.get("GOOGLE_API_KEY", "NOT SET")
+    logger.info(f"Google API Key: {google_api_key}")
+
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8086)
 
